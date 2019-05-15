212b955c39cbeeb528a7a432258f8f98dc786f81
Alex Malikovandr <alex.malikov94@gmail.com>
2019-05-15 16:49:11 +0500

    Choose type variation change in client side

--- pages/products/_slug.vue
+++ pages/products/_slug.vue
@@ -26,7 +26,22 @@
                          :type="type"
                          :variations="variations"
                          :key="type"
+                         v-model="form.variation"
                       	 />
+
+
+                      	 <div class="field has-addons" v-if="form.variation">
+                      	 	<div class="control">
+                      	 		<div class="select is-fullwidth">
+                      	 			<select name="" id="">
+                      	 				<option value="">1</option>
+                      	 			</select>
+                      	 		</div>
+                      	 	</div>
+                      	 	<div class="control">
+                      	 		<button type="submit" class="button is-info">Add to cart</button>
+                      	 	</div>
+                      	 </div>
                       </form>
 					</section>
 				</div>
@@ -39,7 +54,11 @@ import ProductVariation from '@/components/products/ProductVariation'
 	export default {
 		data() {
 			return {
-				product: null
+				product: null,
+				form: {
+					variation: '',
+					quantity: 1
+				}
 			}
 		},
 		components: {
