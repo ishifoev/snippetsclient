6e5b9d5fd4cb0d82797001fd43d82ff90c5146db
Alex Malikovandr <alex.malikov94@gmail.com>
2019-05-23 15:09:23 +0500

    showing if a product is out of stock

--- pages/products/_slug.vue
+++ pages/products/_slug.vue
@@ -14,6 +14,11 @@
 							{{ product.description }}
 						</p>
 						<hr>
+
+						<span class="tag is-rounded is-medium is-dark" v-if="!product.in_stock">
+							Out of Stock
+						</span>
+
 						<span class="tag is-rounded is-medium">
 							{{ product.price }}
 						</span>
